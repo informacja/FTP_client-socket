bebfa1a293d6c39e474e6aa6570657728e8dda55
informacja <piotrwzst@gmail.com>
2017-08-24 09:16:42 +0200

    Alpha version
    TO DO: save file only by FILE* without streams

---
+++ ftp.h
@@ -0,0 +1,273 @@
+#ifndef FTP_H_INCLUDED
+#define FTP_H_INCLUDED
+
+#include <winsock2.h>
+#include <WS2tcpip.h>
+#include <fstream>
+#include <iostream>
+
+/*
+ *  Code below from
+ *  https://github.com/pedro-vicente/lib_netsockets/blob/master/src/ftp.cc
+ *  https://github.com/pedro-vicente/lib_netsockets/blob/master/examples/ftp_client.cc
+ */
+
+
+void send_all(int sock, const void *vbuf, size_t size_buf)
+{
+  const char *buf = (char*)vbuf;	// can't do pointer arithmetic on void*
+  int send_size; // size in bytes sent or -1 on error
+  size_t size_left; // size left to send
+  const int flags = 0;
+
+  size_left = size_buf;
+  while (size_left > 0)
+  {
+    if ((send_size = send(sock, buf, size_left, flags)) == -1)
+    {
+      std::cout << "send error: " << std::endl;
+    }
+    size_left -= send_size;
+    buf += send_size;
+  }
+  return;
+}
+
+// ----------------------------------------------------------------------------
+
+void get_response(int sock, std::string &str_rep)
+{
+  int recv_size; // size in bytes received or -1 on error
+  const int flags = 0;
+  const int size_buf = 1024;
+  char buf[size_buf];
+
+  if ((recv_size = recv(sock, buf, size_buf, flags)) == -1)
+  {
+    std::cout << "recv error: " << strerror(errno) << std::endl;
+  }
+
+  std::string str(buf, recv_size);
+  std::cout << "response: " << str;
+  str_rep = str;
+  return;
+}
+
+// ----------------------------------------------------------------------------
+
+void send_request(int sock, const char* buf_request)
+{
+  std::cout << "request: " << buf_request << std::endl;
+
+  //send
+  send_all(sock, (void *)buf_request, strlen(buf_request));
+  return;
+}
+
+// ----------------------------------------------------------------------------
+
+int login( SOCKET sock, const char *user_name, const char *pass, int port = 21 )
+{
+  char buf_request[255];
+  std::string str_server_ip;
+  std::string str_rsp;
+
+  //create the control socket
+//  create_socket(sock, m_server_ip.c_str(), m_server_port);
+
+  get_response(sock, str_rsp);
+
+  //construct USER request message using command line parameters
+  //Note: there is no space between the user name and CRLF; example of request is "USER me\r\n"
+  sprintf(buf_request, "USER %s\r\n", user_name);
+
+  //send USER request
+  send_request(sock, buf_request);
+
+  //receive response
+  get_response(sock, str_rsp);
+
+  //construct PASS request message using command line parameters
+  sprintf(buf_request, "PASS %s\r\n", pass);
+
+  //send PASS request
+  send_request(sock, buf_request);
+
+  //receive response
+  get_response(sock, str_rsp);
+}
+
+// ----------------------------------------------------------------------------
+
+void parse_PASV_response(const std::string &str_rsp, std::string &str_server_ip, unsigned short &server_port)
+{
+  unsigned int h[4];
+  unsigned int p[2];
+  char server_ip[100];
+
+  size_t pos = str_rsp.find('(');
+  std::string  str_ip = str_rsp.substr(pos + 1);
+  sscanf(str_ip.c_str(), "%u,%u,%u,%u,%u,%u", &h[0], &h[1], &h[2], &h[3], &p[0], &p[1]);
+  server_port = static_cast<unsigned short>(p[0] * 256 + p[1]);
+  sprintf(server_ip, "%u.%u.%u.%u", h[0], h[1], h[2], h[3]);
+  str_server_ip = server_ip;
+}
+
+// ----------------------------------------------------------------------------
+
+void create_socket(int &sock, const char* server_ip, const unsigned short server_port)
+{
+  // server address
+  struct sockaddr_in server_addr;
+
+  // construct the server address structure
+  memset(&server_addr, 0, sizeof(server_addr));// zero out structure
+  server_addr.sin_family = AF_INET;// internet address family
+  server_addr.sin_addr.s_addr = inet_addr(server_ip);// server IP address
+  server_addr.sin_port = htons(server_port);// server port
+
+  // create a stream socket using TCP
+  if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+  {
+  }
+
+  // establish the connection to the server
+  if (connect(sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
+  {
+    std::cout << "connect error: " << strerror(errno) << std::endl;
+    exit(1);
+  }
+}
+
+// ----------------------------------------------------------------------------
+
+void receive_all(int sock, const char *file_name)
+{
+  int recv_size; // size in bytes received or -1 on error
+  const int flags = 0;
+  const int size_buf = 4096;
+  char buf[size_buf];
+  FILE *file;
+
+    std::ofstream fout;
+    sprintf(buf, "STREAM%s", file_name);
+    fout.open( buf, std::ios::out);
+
+  file = fopen( file_name, "wb");
+
+  while (1)
+  {
+    if ((recv_size = recv(sock, buf, size_buf, flags)) == -1)
+    {
+      std::cout << "recv error: " << strerror(errno) << std::endl;
+    }
+    if (recv_size == 0)
+    {
+      std::cout << "all bytes received " << std::endl;
+      break;
+    }
+
+    fwrite(buf, recv_size, 1, file);
+    fout << buf;
+    recv_size = 0;
+  }
+  fout.close();
+  fclose(file);
+
+/// przerobiæ na  fstream
+    recv_size = 2191;
+//
+//    int file_desc = 0;
+//
+//    file_desc = open(file_name, O_CREAT  | O_WRONLY , 0666);
+////    recv(sock, buf, file_size, 0);
+//    write(file_desc, buf, recv_size);
+//    close(file_desc);
+//std::cout << buf;
+
+
+
+  return;
+}
+
+// ----------------------------------------------------------------------------
+
+void close_socket(int sock)
+{
+#if defined (_MSC_VER)
+  closesocket(sock);
+#else
+  ::close(sock);
+#endif
+}
+
+// ----------------------------------------------------------------------------
+
+void get_file( SOCKET sock_ctrl, const char *file_name)
+{
+  char buf_request[255];
+  std::string str_server_ip = "127.0.0.1";
+  std::string str_rsp;
+  unsigned short server_port = 21;
+
+  //The FTP command, SIZE OF FILE (SIZE), is used to obtain the transfer
+  //size of a file from the server-FTP process.This is the exact number
+  //of octets(8 bit bytes) that would be transmitted over the data
+  //connection should that file be transmitted.
+  //syntax: "SIZE" SP pathname CRLF
+  //get SIZE
+  sprintf(buf_request, "SIZE %s\r\n", file_name);
+
+  //send SIZE request
+  send_request(sock_ctrl, buf_request);
+
+  //get response on control socket
+  get_response(sock_ctrl, str_rsp);
+
+  //parse the file size
+  std::string  str_code = str_rsp.substr(0, 3);
+  unsigned long long size_file = 0;
+  if ("213" == str_code)
+  {
+    std::string  str_size = str_rsp.substr(4, str_rsp.size() - 2 - 4); //subtract end CRLF plus start of "213 ", 213 SP
+    size_file = atoi(str_size.c_str());
+
+    std::cout << "FILE transfer is " << size_file << " bytes" << std::endl;
+  }
+
+  //enter passive mode: make PASV request
+  sprintf(buf_request, "PASV\r\n");
+
+  //send PASV request
+  send_request(sock_ctrl, buf_request);
+
+  //get response on control socket
+  get_response(sock_ctrl, str_rsp);
+
+  //parse the PASV response
+  parse_PASV_response(str_rsp, str_server_ip, server_port);
+    int sock_data;
+  //create the data socket
+  create_socket( sock_data, str_server_ip.c_str(), server_port);
+
+  //construct RETR request message
+  sprintf(buf_request, "RETR %s\r\n", file_name);
+
+  //send RETR request on control socket
+  send_request(sock_ctrl, buf_request);
+
+  //get response on control socket
+  get_response(sock_ctrl, str_rsp);
+
+  //get the file (data socket), save to local file with same name
+  receive_all(sock_data, file_name);
+
+  //get response
+  get_response(sock_ctrl, str_rsp);
+
+  //close data socket
+  close_socket(sock_data);
+}
+
+
+#endif // FTP_H_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,444 @@
+#include <stdio.h>        /* for printf() and fprintf() */
+//#include <socket2.h>   /* for socket(), connect(), send(), and recv() */
+
+//#include <arpa/inet.h>    /* for sockaddr_in and inet_addr() */ winsock2
+#include <stdlib.h>       /* for atoi() */
+#include <string.h>       /* for memset() */
+#include <unistd.h>       /* for close() */
+#define RCVBUFSIZE 32     /* Size of receive buffer */
+//#include <netdb.h>
+//#include "in"
+#include <sys/types.h> /* See NOTES */
+#include <sys/stat.h>
+
+#include <iostream>
+//#include <socsys/socket.h>
+//#include <arpa/inet.h>
+//#include <sys/stat.h>
+//#include <sys/sendfile.h>
+#include <fcntl.h>
+//#include "socket.hh"
+#include "ftp.h"
+
+// ----------------------------------------------------------------------------
+
+
+using namespace std;
+
+//#define FILENAME 	"1.txt"
+
+
+// ----------------------------------------------------------------------------
+
+int ftpSendFile(char * buf, char * host, int port);
+int ftpRecvResponse(int sock, char * buf);
+int main(int argc, char *argv[])
+{
+    int sock;                          /*  Socket descriptor */
+    int dataSock;              /* Data sockeet Descriptor*/
+    int numberOfConn;
+    struct sockaddr_in ftpServAddr;   /*  server address */
+    struct sockaddr_in dataServAddr;  /* data address*/
+    struct hostent *pt_clientAddress;
+    unsigned short ftpServPort;       /*  server port */
+    unsigned short dataServPort;
+    unsigned int PORTBUFFER = 256;
+    char *servlP     = new char[256];                      /*  Server IP address (dotted quad) */
+    char *host_name  = new char[256];
+    char *echoString = new char[256];                 /*  String to send to echo server */
+    char *command    = new char[256];
+    char *p          = new char[256];
+    char echoBuffer[RCVBUFSIZE];       /* Buffer for echo string */
+    unsigned int echoStringLen;        /* Length of string to echo */
+    int bytesRcvd, totalBytesRcvd;     /* Bytes read in single recv()
+                                           and total bytes read */
+//host_name = "puler";
+                                 /* First arg' server IP address (dotted quad) */
+/*servlP = argv[0] ;*/
+ servlP = "217.117.129.205";                                /* Second arg' string to echo */
+ servlP = "127.0.0.1";                                /* Second arg' string to echo */
+
+/*echoString = argv[2] ;*/
+char buf[1024];
+
+//  const char *host_name = NULL; // server name
+//  const char *user_name = NULL; // user name
+  const char *pass = "test"; // password
+
+ftpServPort = 21;
+//ftpServPort = 2731;
+printf(servlP);
+
+    WSADATA Data;
+    WSAStartup(MAKEWORD(2, 2), &Data); // 2.2 version
+
+    printf("\nCreating Socket\n");
+/* Create a reliable, stream socket using TCP */
+if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+     printf(" socket () failed %d\n", sock) ;
+
+/*Create a data socket for ftp file transfer */
+if ((dataSock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+     printf(" data socket () failed\n") ;
+
+/* Construct the server address structure */
+memset(&ftpServAddr, 0, sizeof(ftpServAddr));         /* Zero out structure */
+ftpServAddr.sin_family         = AF_INET;              /* Internet address family */
+ftpServAddr.sin_addr.s_addr     = inet_addr(servlP);       /* Server IP address */
+ftpServAddr.sin_port           = htons(ftpServPort); /* Server port */
+
+/* Establish the connection to the echo server */
+if (connect(sock, (struct sockaddr *) &ftpServAddr, sizeof(ftpServAddr)) < 0)
+     printf(" connect () failed\n") ;
+else
+    printf("Connection Successful\n");
+
+//login(sock,host_name,"tedst");
+
+
+dataServPort = 20;
+/* Construct the server address structure */
+memset(&dataServAddr, 0, sizeof(dataServAddr));         /* Zero out structure */
+dataServAddr.sin_family         = AF_INET;              /* Internet address family */
+dataServAddr.sin_addr.s_addr    = htonl(INADDR_ANY);       /* Server IP address */
+dataServAddr.sin_port           = htons(dataServPort); /* Server port */
+
+/* BIND socket to (sockaddr_in) clientAddr */
+bind(dataSock,(struct sockaddr *) &dataServAddr, sizeof(dataServAddr));
+
+/* LISTEN on data_socket */
+listen(dataSock, numberOfConn);
+
+gethostname(host_name, PORTBUFFER);
+cout << "hostname (" << host_name << "): ";
+string buff = "";
+//getline(std::cin, buff);
+if ( buff != "" )
+    strcpy( host_name, (const char*) &buff);
+                                            host_name = "test";
+printf("*******PORT********\n");
+printf(host_name);
+printf("\n*******\n");
+/* Determine input length */
+echoStringLen = strlen(echoString) ;
+printf("Sending HTTP Request\n");
+printf(echoString);
+
+login( sock, host_name, pass );
+
+//system("pause");
+//buff = string ( "USER " + string(*host_name)+ "\r\n");
+//p = strcpy(command, "USER test\r\n");
+//send(sock, command, strlen(command), 0);
+//ftpRecvResponse(sock, buf);
+//
+//p = strcpy(command, "PASS test\r\n");
+//send(sock, command, strlen(command), 0);
+//ftpRecvResponse(sock, buf);
+//
+//p = strcpy(command, "CWD /\r\n");
+//send(sock, command, strlen(command), 0);
+//ftpRecvResponse(sock, buf);
+//
+p = strcpy(command, "WebGrabber.exe");                                         /// wybieramy co pobraæ
+get_file(sock, command);
+//
+//p = strcpy(command, "size 1.txt\r\n");
+//send(sock, command, strlen(command), 0);
+//ftpRecvResponse(sock, buf);
+
+//p = strcpy(command, "CWD /\r\n");
+//send(sock, command, strlen(command), 0);
+//ftpRecvResponse(sock, buf);
+
+//
+//p = strcpy(command, "TYPE I\r\n");
+//send(sock, command, strlen(command), 0);
+//ftpRecvResponse(sock, buf);
+
+//p= strcpy(command, "PASV\r\n");
+//send(sock, command, strlen(command), 0);
+//ftpRecvResponse(sock, buf);
+
+
+//p=strcpy(command, "RETR 1.txt\r\n");
+//send(sock, command, strlen(command), 0);
+//char *host;
+//int tmpPort;
+//host = "test";
+//tmpPort = 20;
+
+//ftpRecvResponse(sock, buf);
+
+//
+//p = strcpy(command, "PASV \r\n");
+//send(sock, command, strlen(command), 0);
+//ftpRecvResponse(sock, buf);
+//
+//p = strcpy(command, "RETR SAM_7383.JPG\r\n");
+//send(sock, command, strlen(command), 0);
+//ftpRecvResponse(sock, buf);
+
+//string  buff;
+
+//cout << "\nType \"exit\" to end";
+////
+////while ( buff != "exit" )
+////{
+//    buff = "";
+//    cout << "\nPolecenie: ";
+//    cin >> buff;
+////    p = strcpy(command, (const char*) &buff);
+////    *command = buff.c_str();
+//    std::copy(buff.begin(), buff.end(), command);
+//    command[buff.size()] = '\0';
+//
+////    p = strcpy(command, buff.c_str());
+//    send(sock, command, strlen(command), 0);
+//    ftpRecvResponse(sock, buf);
+//system("pause");
+
+// Get a file from server
+	strcpy(command, "Get ");
+	strcat(command, FILENAME);
+	p = strcpy(command, "RECV 1.txt");
+    send(dataSock, command, strlen(command), 0);
+//	write(dataSock, command, strlen(command) );
+//ftpRecvResponse(dataSock, buf);
+	recv(dataSock, buf, 2, 0);
+//	// Variables for the file being received
+
+	int	file_size = 0,
+		file_desc = 0;
+	char	*data;
+
+	// Start receiving file
+	if (strcmp(buf, "OK") != 0) {
+
+		recv(dataSock, (char*)&file_size, sizeof(int), 0);
+		file_size = 2131291;
+		data = (char*)malloc(file_size);
+//		file_desc = open(FILENAME, O_CREAT  | O_WRONLY | O_APPEND, 0666);
+		file_desc = open(FILENAME, O_CREAT  | O_WRONLY , 0666);
+		recv(dataSock, data, file_size, 0);
+		write(file_desc, data, file_size);
+		close(file_desc);
+	}
+	else {
+
+		fprintf(stderr, "Bad request\n");
+	}
+cout << "\nLast Err: " << WSAGetLastError();
+//system("pause");
+//p = strcpy(command,"MODE S\r\n");
+//send(sock,command, strlen(command), 0);
+//ftpRecvResponse(sock, buf);
+//p = strcpy(command,"TYPE A\r\n");
+//send(sock,command, strlen(command), 0);
+//ftpRecvResponse(sock, buf);
+//p= strcpy(command, "STRU F\r\n");
+//send(sock, command, strlen(command), 0);
+//ftpRecvResponse(sock, buf);
+//p = strcpy(command,"LIST \r\n");
+//send(sock,command, strlen(command), 0);
+//ftpRecvResponse(sock, buf);
+//p= strcpy(command, "PASV \r\n");
+//send(sock, command, strlen(command), 0);
+//ftpRecvResponse(sock, buf);
+
+//if (connect(dataSock, (struct sockaddr *) &dataServAddr, sizeof(dataServAddr)) < 0)
+//     printf(" connect () failed\n") ;
+//else
+//    printf("Connection Successful\n");
+
+
+//p=strcpy(command, "RETR 1.txt\r\n");
+//send(sock, command, strlen(command), 0);
+//char *host;
+//int tmpPort;
+//host = "test";
+//tmpPort = 20;
+//
+//ftpRecvResponse(sock, buf);
+*buf= 'T';
+//ftpSendFile(buf,host_name,20);
+
+printf("\n");     /* Print a final linefeed */
+
+delete servlP;
+delete host_name;
+delete echoString;
+delete command;
+delete p;
+
+  close(sock);
+  WSACleanup();
+  return 0;
+//  exit(0);
+
+}
+int ftpSendFile(char * buf, char * host, int port) {
+   int sd;
+   struct sockaddr_in pin;
+   struct hostent *hp;
+
+   if ((hp = gethostbyname(host)) == 0) {
+      perror("gethostbyname");
+      return -1;
+   }
+   memset(&pin, 0, sizeof(pin));
+   pin.sin_family = AF_INET;
+   pin.sin_addr.s_addr = ((struct in_addr *)(hp->h_addr))->s_addr;
+   pin.sin_port = htons(port);
+   if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+      perror("socket");
+      return -1;
+   }
+   if (connect(sd,(struct sockaddr *)  &pin, sizeof(pin)) == -1) {
+      perror("connect");
+      return -1;
+   }
+   if(send(sd, buf, strlen(buf), 0) == -1) {
+      perror("send");
+      return -1;
+   }
+   /*closesocket(sd); //close the socket*/
+   return 0;
+}
+int ftpRecvResponse(int sock, char * buf) {
+   int i;
+
+   if (recv(sock, buf, 5120, 0) == -1) {//receive the data
+      perror("recv");
+      return 0;;
+   }
+
+//   for(i=(strlen(buf) - 1);i>0;i--) {
+//      if(buf[i]=='.' || buf[i]=='\r') {
+//         buf[i+1]='\0';
+//         break;
+//      }
+//   }
+
+//// Fixed version by Puler
+//    for( i = 0; i < strlen(buf)-1; i++ )
+//    {
+//        if( buf[i]=='.' || buf[i]=='\r' ) {
+//            buf[i+1]='\0';
+//            break;
+//        }
+//    }
+
+
+    HANDLE  hConsole;
+
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, 0x08);
+//   printf("\033[32;1m %s \033[0m", buf); //print response to the screen
+    cout << buf;
+//   printf("%s", buf); //print response to the screen
+    SetConsoleTextAttribute(hConsole, 0x07);
+
+// Beter Fixed version by Puler
+   unsigned length = strlen(buf);
+    for( i = 0; i < length; i++ )
+    {
+        buf[i]='\0';
+    }
+
+   return 0;
+}
+/*
+ *  client.c
+ *
+ *  Simple FTP Client
+ *  ====================
+ *  Connects the server, and sends a command "Get FILENAME" to retrieve
+ *  the file. If the file exists on the server, client retrieves it.
+ *
+ *  Compile & link  :   gcc client.c -o client
+ *  Execute         :   ./client
+ *
+ *  Ozcan Ovunc <ozcan_ovunc@hotmail.com>
+ */
+//
+//#include <stdio.h>
+////#include <socsys/socket.h>
+////#include <arpa/inet.h>
+////#include <sys/stat.h>
+////#include <sys/sendfile.h>
+//#include <winsock2.h>
+//#include <WS2tcpip.h>
+//#include <fcntl.h>
+//#include <stdlib.h>
+//#include <string.h>
+//
+//#define FILENAME 	"a.txt"
+//#define SERVER_IP 	"127.0.0.1"
+//#define SERVER_PORT 	21
+//
+//int main(int argc , char **argv)
+//{
+//    WSADATA Data;
+////   int socket; // or you can use SOCKET socket
+//   WSAStartup(MAKEWORD(2, 2), &Data); // 2.2 version
+//   int  socket1; socket1 = socket(AF_INET, SOCK_DGRAM, 0);
+//   if(socket1<0){
+//        printf("Error Creating Socket");
+//   }else{
+//        printf("Successfully Created Socket");
+//   }
+//   system("pause");
+//
+//
+//	int 	socket_desc;
+//	struct 	sockaddr_in server;
+//	char 	request_msg[BUFSIZ],
+//		reply_msg[BUFSIZ];
+//
+//	// Variables for the file being received
+//	int	file_size,
+//		file_desc;
+//	char	*data;
+//
+//	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+//	if (socket_desc == -1)
+//	{
+//		perror("Could not create socket");
+//		return 1;
+//	}
+//
+//	server.sin_addr.s_addr = inet_addr(SERVER_IP);
+//	server.sin_family = AF_INET;
+//	server.sin_port = htons(SERVER_PORT);
+//
+//	// Connect to server
+//	if (connect(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0)
+//	{
+//		perror("Connection failed");
+//		return 1;
+//	}
+//
+//	// Get a file from server
+//	strcpy(request_msg, "Get ");
+//	strcat(request_msg, FILENAME);
+//	write(socket_desc, request_msg, strlen(request_msg));
+//	recv(socket_desc, reply_msg, 2, 0);
+//
+//	// Start receiving file
+//	if (strcmp(reply_msg, "OK") == 0) {
+//
+//		recv(socket_desc, (char*)&file_size, sizeof(int), 0);
+//		data = (char*)malloc(file_size);
+//		file_desc = open(FILENAME, O_CREAT | O_EXCL | O_WRONLY, 0666);
+//		recv(socket_desc, data, file_size, 0);
+//		write(file_desc, data, file_size);
+//		close(file_desc);
+//	}
+//	else {
+//
+//		fprintf(stderr, "Bad request\n");
+//	}
+//
+//	return 0;
+//}
